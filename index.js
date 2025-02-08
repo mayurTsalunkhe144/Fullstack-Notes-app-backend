19d4ae3ac1c57d3ef7d977554535bae958b4d09d
Mayur <mayurtsalunkhe@gmail.com>
2025-02-09 01:30:07 +0530

    first commit

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 import express from 'express';
 import cors from 'cors';
+import users from './routes/users.js';
+
 
 const app = express();
+app.use("/api/users",users)
 app.use(cors());
 
 app.get( '/', ( req, res ) => {
