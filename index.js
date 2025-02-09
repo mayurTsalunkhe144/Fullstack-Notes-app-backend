a450e69ec95d3bdcdb2e22c66dbdafa07ea6c347
Mayur <mayurtsalunkhe@gmail.com>
2025-02-09 19:41:48 +0530

    added cors

--- index.js
+++ index.js
@@ -7,12 +7,32 @@ const app = express();
 app.use("/api/users",users)
 app.use(cors());
 
-app.get( '/', ( req, res ) => {
-    res.send( 'Hello, World! backend working' );
+app.use(
+    cors({
+      origin: 'http://localhost:3002', // Allow only this origin
+      methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allow only these methods
+      allowedHeaders: ['Content-Type', 'Authorization'], // Allow only these headers
+      credentials: true, // Allow cookies and credentials
+    })
+  );
+
+  app.use(
+    cors({
+      origin: 'https://fullstack-notes-app-frontend.vercel.app', // Allow only this origin
+      methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allow only these methods
+      allowedHeaders: ['Content-Type', 'Authorization'], // Allow only these headers
+      credentials: true, // Allow cookies and credentials
+    })
+  );
+
+app.get( '/',( req, res ) => {
+    res.json({
+        message: 'Hello from server',
+    });
 })
 
-app.listen( 3000, () => {
-    console.log('Server is running on port 3000');
+app.listen( 3001, () => {
+    console.log('Server is running on port 3001');
 })
 
  
--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,20 @@
 import express from 'express';
+import cors from 'cors';
 const router= express.Router();
 
+router.use(
+    cors({
+      origin: 'http://localhost:3000', // Allow only this origin
+      methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allow only these methods
+      allowedHeaders: ['Content-Type', 'Authorization'], // Allow only these headers
+      credentials: true, // Allow cookies and credentials
+    })
+  );
+
 router.get("/",(req,res)=>{
-    res.send("Hello from user route");
+    res.json({
+        message:"Welcome to the users route"
+    });
 }
 )
 
