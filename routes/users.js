2d044adb3c529a54ba3b18a1f365f957862bc68e
Mayur <mayurtsalunkhe@gmail.com>
2025-02-09 19:52:59 +0530

    added cors

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,14 @@ import express from 'express';
 import cors from 'cors';
 const router= express.Router();
 
-
+router.use(
+    cors({
+      origin: 'https://fullstack-notes-app-frontend.vercel.app', // Allow only this origin
+      methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allow only these methods
+      allowedHeaders: ['Content-Type', 'Authorization'], // Allow only these headers
+      credentials: true, // Allow cookies and credentials
+    })
+  );
 
 router.get("/",(req,res)=>{
     res.json({
