cfd27369a1a189e153cf0af370153928d2ef663a
Mayur <mayurtsalunkhe@gmail.com>
2025-02-09 19:50:14 +0530

    added cors

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(cors());
 
 app.get( '/',( req, res ) => {
     res.json({
-        message: 'Hello from server',
+        message: 'server is working fine',
     });
 })
 
--- routes/users.js
+++ routes/users.js
@@ -13,7 +13,7 @@ router.use(
 
 router.get("/",(req,res)=>{
     res.json({
-        message:"Welcome to the users route"
+        message:"Welcome to the users route its working fine"
     });
 }
 )
