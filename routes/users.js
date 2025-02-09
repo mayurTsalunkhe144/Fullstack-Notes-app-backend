c23dfc17c991253763e78a7d94ea8d40c02335c8
Mayur <mayurtsalunkhe@gmail.com>
2025-02-09 19:51:19 +0530

    added cors

--- routes/users.js
+++ routes/users.js
@@ -2,14 +2,7 @@ import express from 'express';
 import cors from 'cors';
 const router= express.Router();
 
-router.use(
-    cors({
-      origin: 'http://localhost:3000', // Allow only this origin
-      methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allow only these methods
-      allowedHeaders: ['Content-Type', 'Authorization'], // Allow only these headers
-      credentials: true, // Allow cookies and credentials
-    })
-  );
+
 
 router.get("/",(req,res)=>{
     res.json({
